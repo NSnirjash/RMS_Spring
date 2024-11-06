128f75b651be9c22aa10e8bd8d63251afac23d6d
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-11-06 18:35:57 +0600

    update configaration

--- RMSSpring/src/main/java/com/Project/RMSSpring/security/SecurityConfig.java
+++ RMSSpring/src/main/java/com/Project/RMSSpring/security/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8091"));  // Add allowed origins
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8091","https://8ccf-103-205-69-8.ngrok-free.app"));  // Add allowed origins
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
